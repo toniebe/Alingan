4d57ce44f55df6ade20c37ae5ae402904815262e
toniebe <toniebe48@gmail.com>
2022-08-11 18:20:11 +0700

    inisialize project

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native';
+
+
+export default function Router() {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
