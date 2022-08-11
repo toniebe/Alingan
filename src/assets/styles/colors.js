4364b637770b74ffe5ac84a558c69c68a563acf7
toniebe <toniebe48@gmail.com>
2022-08-11 20:03:49 +0700

    feat: create styles (font and color)

---
+++ src/assets/styles/colors.js
@@ -0,0 +1,168 @@
+import {StyleSheet} from 'react-native';
+
+// Neutral
+const CNeutral = '#FFFFFF';
+const CNeutral1 = '#FAFAFA';
+const CNeutral2 = '#F5F5F5';
+const CNeutral3 = '#F0F0F0';
+const CNeutral4 = '#D9D9D9';
+const CNeutral5 = '#BFBFBF';
+const CNeutral6 = '#8C8C8C';
+const CNeutral7 = '#595959';
+const CNeutral8 = '#434343';
+const CNeutral9 = '#262626';
+const CNeutral10 = '#1F1F1F';
+const CNeutral11 = '#141414';
+const CNeutral12 = '#000000';
+
+// TextLight
+
+const CTextLight = '#FAFAFA';
+const CTextLight1 = '#F5F5F5';
+const CTextLight2 = '#F0F0F0';
+const CTextLight3 = '#D9D9D9';
+const CTextLight4 = '#BFBFBF';
+const CTextLight5 = '#8C8C8C';
+const CTextLight6 = '#595959';
+const CTextLight7 = '#262626';
+
+// TextDark
+
+const CTextDark = '#1D1D1D';
+const CTextDark1 = '#262626';
+const CTextDark2 = '#303030';
+const CTextDark3 = '#434343';
+const CTextDark4 = '#5A5A5A';
+const CTextDark5 = '#7D7D7D';
+const CTextDark6 = '#ACACAC';
+const CTextDark7 = '#DBDBDB';
+
+// Primary
+const CPrimary = '#D2512C';
+
+const colors = StyleSheet.create({
+  Neutral: {
+    color: CNeutral,
+  },
+  Neutral1: {
+    color: CNeutral1,
+  },
+  Neutral2: {
+    color: CNeutral2,
+  },
+  Neutral3: {
+    color: CNeutral3,
+  },
+  Neutral4: {
+    color: CNeutral4,
+  },
+  Neutral5: {
+    color: CNeutral5,
+  },
+  Neutral6: {
+    color: CNeutral6,
+  },
+  Neutral7: {
+    color: CNeutral7,
+  },
+  Neutral8: {
+    color: CNeutral8,
+  },
+  Neutral9: {
+    color: CNeutral9,
+  },
+  Neutral10: {
+    color: CNeutral10,
+  },
+  Neutral11: {
+    color: CNeutral11,
+  },
+  Neutral12: {
+    color: CNeutral12,
+  },
+  TextLight: {
+    color: CTextLight,
+  },
+  TextLight1: {
+    color: CTextLight1,
+  },
+  TextLight2: {
+    color: CTextLight2,
+  },
+  TextLight3: {
+    color: CTextLight3,
+  },
+  TextLight4: {
+    color: CTextLight4,
+  },
+  TextLight5: {
+    color: CTextLight5,
+  },
+  TextLight6: {
+    color: CTextLight6,
+  },
+  TextLight7: {
+    color: CTextLight7,
+  },
+  TextDark: {
+    color: CTextDark,
+  },
+  TextDark1: {
+    color: CTextDark1,
+  },
+  TextDark2: {
+    color: CTextDark2,
+  },
+  TextDark3: {
+    color: CTextDark3,
+  },
+  TextDark4: {
+    color: CTextDark4,
+  },
+  TextDark5: {
+    color: CTextDark5,
+  },
+  TextDark6: {
+    color: CTextDark6,
+  },
+  TextDark7: {
+    color: CTextDark7,
+  },
+  Primary: {
+    color: CPrimary,
+  },
+});
+
+export {
+  CNeutral,
+  CNeutral1,
+  CNeutral2,
+  CNeutral3,
+  CNeutral4,
+  CNeutral5,
+  CNeutral6,
+  CNeutral7,
+  CNeutral8,
+  CNeutral9,
+  CNeutral10,
+  CNeutral11,
+  CNeutral12,
+  CPrimary,
+  CTextLight,
+  CTextLight1,
+  CTextLight2,
+  CTextLight3,
+  CTextLight4,
+  CTextLight5,
+  CTextLight6,
+  CTextLight7,
+  CTextDark,
+  CTextDark1,
+  CTextDark2,
+  CTextDark3,
+  CTextDark4,
+  CTextDark5,
+  CTextDark6,
+  CTextDark7,
+  colors,
+};
---
+++ src/assets/styles/font.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+const font = StyleSheet.create({
+    Poppins:{
+        fontFamily:'Poppins-Black'
+    },
+    PoppinsBold:{
+        fontFamily:'Poppins-Bold'
+    }
+})
+
+export default font;
