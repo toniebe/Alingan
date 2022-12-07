f07359e87dd35d5d67f1a7c5b6f8b5295be2c5ca
ebetonii <ahmad.fathoni@klikcair.com>
2022-12-07 20:04:14 +0700

    fix: adding plotlist in new enviroment ios

--- android/app/src/main/java/com/alingan/MainApplication.java
+++ android/app/src/main/java/com/alingan/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
 import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
+import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
